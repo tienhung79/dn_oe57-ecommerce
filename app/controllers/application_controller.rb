eab5d0092d89302f443216522eac12fc29562b6d
hpt1512 <hoangphuctanpt@gmail.com>
2023-09-08 11:22:43 +0700

    Create home page layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,11 @@
 class ApplicationController < ActionController::Base
-  around_action :switch_locale
+  # app/controllers/application_controller.rb
+  before_action :set_locale
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
-  def switch_locale &action
-    locale = params[:locale] || I18n.default_locale
-    I18n.with_locale(locale, &action)
+  def default_url_options
+    {locale: I18n.locale}
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module RailsTutorial
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    # config/application.rb
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
+    # config/application.rb
+    config.i18n.available_locales = [:en, :vi]
+    # config/application.rb
+    config.i18n.default_locale = :vi
 
     # Configuration for the application, engines, and railties goes here.
     #
